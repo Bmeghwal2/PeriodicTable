701d7af336e5065ae8be829a27d59048b12deb40
Bmeghwal2 <bmeghwal2@gmail.com>
2023-01-04 13:00:06 +0000

    chore: final edit with successful run

--- element.sh
+++ element.sh
@@ -11,10 +11,10 @@ INPUT=$1
 else
 INPUT=$($PSQL "select atomic_number from elements where symbol='$1' or name='$1'")
 fi
-GET_ALL_DATA=$($PSQL "select name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type, atomic_number from elements full join properties using(atomic_number) full join types using(type_id) where atomic_number='$INPUT'")
-echo $GET_ALL_DATA | while IFS=" | " read NAME SYMBOL ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE ATOMIC_NUMBER
+READ_ALL_DATA=$($PSQL "select name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type, atomic_number from elements full join properties using(atomic_number) full join types using(type_id) where atomic_number='$INPUT'")
+echo $READ_ALL_DATA | while IFS=" | " read NAME SYMBOL ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE ATOMIC_NUMBER
 do
-if [[ -z $GET_ALL_DATA ]]
+if [[ -z $READ_ALL_DATA ]]
 then
 echo "I could not find that element in the database."
 else
