861518b93b8b8bd4a96b5814e1e76dfb328ccd67
Bmeghwal2 <bmeghwal2@gmail.com>
2023-01-04 12:51:29 +0000

    refactor: input declared null for values not present in database

--- element.sh
+++ element.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 #read $1
-PSQL="psql -X --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]
 then
 echo Please provide an element as an argument.
 else
 if [[ $1 =~ ^[0-9]+$ ]]
 then
-ATOMIC_NUMBER=$1
+INPUT=$1
 else
-ATOMIC_NUMBER=$($PSQL "select atomic_number from elements where symbol='$1' or name='$1'")
+INPUT=$($PSQL "select atomic_number from elements where symbol='$1' or name='$1'")
 fi
-if [[ -z $ATOMIC_NUMBER ]]
-then 
-echo "I could not find that element in the database."
-else 
-READ_ALL_DATA=$($PSQL "select name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type, atomic_number from elements full join properties using(atomic_number) full join types using(type_id) where atomic_number=$ATOMIC_NUMBER")
+READ_ALL_DATA=$($PSQL "select name, symbol, atomic_mass, melting_point_celsius, boiling_point_celsius, type, atomic_number from elements full join properties using(atomic_number) full join types using(type_id) where atomic_number=$INPUT")
 echo $READ_ALL_DATA | while IFS=" | " read NAME SYMBOL ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE ATOMIC_NUMBER
 do
+if [[ -z $READ_ALL_DATA ]]
+then
+echo "I could not find that element in the database."
+else
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-done
 fi
+done
 fi
